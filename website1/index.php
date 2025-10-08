0a8d60f9bd8266f67c6e1c39f805c32dc974b962
mugunthanarjunan-glitch <mugunthanarjunan@gmail.com>
2025-10-08 14:23:29 +0530

    bootstrap added

--- website1/index.php
+++ website1/index.php
@@ -7,6 +7,7 @@
   <title>Travel Site</title>
   <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -15,8 +16,8 @@
     <div class="navbar">
       <div class="logo">
         <ul>
-          <li>
-          </li>
+          <li><img src="assets/revivelogo.jpg" alt=""></li>
+          <a href="index.php">Revive Holidays</a>
         </ul>
       </div>
       <nav>
@@ -73,5 +74,6 @@
   </section>
 
 </body>
+
 <script src="script.js"></script>
 </html>
